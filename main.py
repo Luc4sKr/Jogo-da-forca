c747c8f70289fb9525860eff8811108b7ab9d2b1
Lucas Kreuch <lucaskreuch77@gmail.com>
2022-04-10 14:15:43 -0300

    Jogo terminado

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Forca:
         self.palavra_sorteada = self.arquivo.readlines()[self.random_num]
         self.palavra_sorteada = self.palavra_sorteada[0:len(self.palavra_sorteada) - 1]
 
-        print(self.palavra_sorteada)
+        #print(self.palavra_sorteada)
 
         for letras_da_palavra in self.palavra_sorteada:
              self.letras_corretas.append("_ ")
@@ -75,7 +75,7 @@ class Forca:
             pygame.display.update()
             pygame.display.flip()
 
-
+    # -- Eventos do jogo -------------------------------------------------------
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -111,11 +111,11 @@ class Forca:
 
                     self.user_text = ""
 
-                # Recebe os inputs das letras ---------------------------------
+                # -- Recebe os inputs das letras ------------------------------
                 else:
                     self.user_text = event.unicode
 
-
+    # -- Eventos de quando o jogo termina -------------------------------------
     def game_over_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -187,112 +187,6 @@ class Forca:
         if self.erros == 0:
             self.draw("X", 10, (255, 255, 255), 186, 120)
             self.draw("X", 10, (255, 255, 255), 196, 120)
-
-
-
-if __name__ == '__main__':
-    forca = Forca()
-import pygame
-from random import randint
-from sys import exit
-
-pygame.init()
-
-class Forca:
-    def __init__(self):
-        self.SCREEN_X, self.SCREEN_Y = 480, 520
-        self.screen = pygame.display.set_mode((self.SCREEN_X, self.SCREEN_Y))
-        pygame.display.set_caption("Jogo da forca")
-
-        self.clock = pygame.time.Clock()
-
-        self.arquivo = open("palavras.txt", "r")
-        self.random_num = randint(0, len(self.arquivo.readlines())-1)
-
-        self.arquivo = open("palavras.txt", "r")
-        self.palavra_sorteada = self.arquivo.readlines()[self.random_num]
-        self.palavra_sorteada = self.palavra_sorteada[0:len(self.palavra_sorteada)-1]
-
-        self.base_font = pygame.font.Font(None, 32)
-        self.user_text = ""
-        self.input_rect = pygame.Rect(self.SCREEN_X/2 - 140/2, 350, 140, 32)
-        self.color = pygame.Color("white")
-
-        print(self.palavra_sorteada)
-
-        self.game_over = False
-        self.jogo()
-
-
-    def jogo(self):
-        self.letras_corretas = []
-        self.letras_erradas = []
-
-        for letras_da_palavra in self.palavra_sorteada:
-             self.letras_corretas.append("_ ")
-
-        while not self.game_over:
-            self.screen.fill((0, 0, 0))
-
-            self.forca()
-
-            self.draw(str(self.letras_corretas).strip("[]").replace("'", ""), 32, (255, 255, 255), self.SCREEN_X/2, 300)
-
-            pygame.draw.rect(self.screen, self.color, self.input_rect, 2)
-            self.text_surface = self.base_font.render(self.user_text, True, (255, 255, 255))
-            self.screen.blit(self.text_surface, (self.SCREEN_X/2, self.input_rect.y + 5))
-
-            self.events()
-
-            pygame.display.update()
-            pygame.display.flip()
-
-
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.game_over = True
-                pygame.quit()
-                exit()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    c = 0
-                    for i in self.palavra_sorteada.upper():
-                        if self.user_text.upper() == i.upper():
-                            self.letras_corretas.insert(c, i)
-                            self.letras_corretas.pop(c+1)
-                        else: # --------------------- !!!!!!!!!!!!!!!!!!!!!!!!
-                            self.letras_erradas.append(self.user_text)
-                            if self.letras_erradas[c] == i:
-                                self.letras_erradas.remove(i)
-                            #print(self.letras_erradas)
-                        c += 1
-                    self.user_text = ""
-                else:
-                    self.user_text = event.unicode
-
-
-    def draw(self, text, tam, color, x, y):
-        self.fonte = pygame.font.Font(pygame.font.match_font("arial"), tam)
-        self.obj = self.fonte.render(text, False, color)
-        self.obj_rect = self.obj.get_rect()
-        self.obj_rect.center = (x, y)
-        self.screen.blit(self.obj, self.obj_rect)
-
-
-    def forca(self):
-        self.draw("|", 100, (255, 255, 255), 100, 100)
-        self.draw("|", 100, (255, 255, 255), 100, 190)
-        self.draw("_", 100, (255, 255, 255), 121, 15)
-        self.draw("_", 100, (255, 255, 255), 168, 15)
-        self.draw("|", 50, (255, 255, 255), 190, 80)
-        self.draw("O", 50, (255, 255, 255), 190, 125)
-        self.draw("|", 50, (255, 255, 255), 190, 160)
-        self.draw("/", 50, (255, 255, 255), 185, 170)
-        self.draw("\\", 50, (255, 255, 255), 195, 170)
-        self.draw("/", 50, (255, 255, 255), 185, 200)
-        self.draw("\\", 50, (255, 255, 255), 195, 200)
 
 
 
